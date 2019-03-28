7f9cc9d85c3e303495799bfe32de8d1c6c431a04
pnminh <pnminh232@gmail.com>
2019-03-27 23:46:00 -0500

    use template driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -18,7 +19,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"]
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild("signupForm") signupForm: NgForm;
+  questions: { key: string; value: string }[] = [
+    { key: "pet", value: "What is your first pet" },
+    { key: "teacher", value: "What is your first teacher" }
+  ];
+  genders: string[] = ["Male", "Female"];
+  defaultQuestionOption = this.questions[0].key;
+  questionAnswer: string;
+  isSubmitted = false;
+  user: {
+    name: string;
+    username: string;
+    password?: string;
+    email: string;
+    question: string;
+    answer: string;
+    gender: string;
+  };
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.defaultQuestionOption);
   }
+  onSubmit() {
+    console.log(this.signupForm);
+    this.isSubmitted = true;
+    this.user = {
+      username: this.signupForm.value.username,
+      name: this.signupForm.value.name,
+      email: this.signupForm.value.email,
+      password: this.signupForm.value.password,
+      question: this.signupForm.value.questionAnswer.secretQuestion,
+      answer:this.signupForm.value.questionAnswer.secretAnswer,
+      gender:this.signupForm.value.gender
+    };
+    this.signupForm.reset();
+    console.log(this.user);
+  }
+  setSuggestedUsername() {
+    this.signupForm.form.patchValue({
+      name: "hero87"
+    });
 
+
+  }
 }
